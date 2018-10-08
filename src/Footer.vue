1f019b6cbefd9ca85dbd5f54f222f3b66f78eeab
Ben Dixon <bendixon311@gmail.com>
2018-10-07 22:16:49 -0600

    fixed footer

--- src/Footer.vue
+++ src/Footer.vue
@@ -2,7 +2,7 @@
 
 <v-toolbar color="indigo" dark>
             
-            <v-toolbar-title>All Servers are managed here</v-toolbar-title>
+            <v-toolbar-title>All Servers are managed here!</v-toolbar-title>
             
             
           </v-toolbar>
